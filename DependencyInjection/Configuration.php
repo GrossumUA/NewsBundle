a3f4a8c1e85d2f874cecc35b4e12992c1b0d8b33
malavsky <malavskuy@gmail.com>
2015-05-07 13:55:12 +0300

    typo fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('grossum_feedback');
+        $rootNode = $treeBuilder->root('grossum_news');
 
         return $treeBuilder;
     }
